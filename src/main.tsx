412c62076e3468bcc98e1f83c335324e7bd393e3
Report-Engainall <ahadnanmo@gmail.com>
2026-09-04 22:16:55 +0300

    ux: include accessibility safeguards in app shell

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import AppErrorBoundary from './AppErrorBoundary';
 import './offline.css';
+import './accessibility.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><AppErrorBoundary><App /></AppErrorBoundary></StrictMode>
