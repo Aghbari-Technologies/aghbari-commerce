a6f8d6c777498dbfc82152afebcf77079ae56300
Report-Engainall <ahadnanmo@gmail.com>
2026-09-04 20:30:02 +0300

    fix: activate global application error recovery

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import AppErrorBoundary from './AppErrorBoundary';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><App /></StrictMode>
+  <StrictMode><AppErrorBoundary><App /></AppErrorBoundary></StrictMode>
 );
 
 if ('serviceWorker' in navigator) {
