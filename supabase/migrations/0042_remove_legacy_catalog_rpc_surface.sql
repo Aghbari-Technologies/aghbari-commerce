f8910909c2adb312e598683f350e8924a33f4b37
Report-Engainall <ahadnanmo@gmail.com>
2026-09-07 04:06:18 +0300

    chore(migrations): renumber legacy catalog revocation

---
+++ supabase/migrations/0042_remove_legacy_catalog_rpc_surface.sql
@@ -0,0 +1,3 @@
+revoke execute on function public.get_catalog(text,uuid,integer,integer) from authenticated;
+revoke execute on function public.get_catalog(text,uuid,integer,integer) from anon;
+revoke execute on function public.get_catalog(text,uuid,integer,integer) from public;
