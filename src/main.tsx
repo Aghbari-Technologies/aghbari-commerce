98c3e6dd19c745402ca560a8e6fb8321975e5cb7
Report-Engainall <ahadnanmo@gmail.com>
2026-09-08 08:57:18 +0300

    feat: mount customer and order workspaces

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import AppErrorBoundary from './AppErrorBoundary';
 import CustomerExperiencePortal from './CustomerExperiencePortal';
+import OrderOperations from './OrderOperations';
 import './offline.css';
 import './accessibility.css';
 import './product-excellence.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><AppErrorBoundary><><App /><CustomerExperiencePortal /></></AppErrorBoundary></StrictMode>
+  <StrictMode><AppErrorBoundary><><App /><CustomerExperiencePortal /><OrderOperations /></></AppErrorBoundary></StrictMode>
 );
 
 if ('serviceWorker' in navigator) {
