68165ac8f1a277cb5aac0856eb3b78240da6ccfa
Report-Engainall <ahadnanmo@gmail.com>
2026-09-08 08:56:59 +0300

    feat: mount customer account workspace

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import AppErrorBoundary from './AppErrorBoundary';
+import CustomerExperiencePortal from './CustomerExperiencePortal';
 import './offline.css';
 import './accessibility.css';
 import './product-excellence.css';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode><AppErrorBoundary><App /></StrictMode>
+  <StrictMode><AppErrorBoundary><><App /><CustomerExperiencePortal /></></AppErrorBoundary></StrictMode>
 );
 
 if ('serviceWorker' in navigator) {
