271c1671382ccc4b1dc4bbf99f74098e8298e61f
Report-Engainall <ahadnanmo@gmail.com>
2026-09-10 03:45:09 +0300

    fix(b2b): point application entry to validated portal

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './AppV3';
+import App from './AppV3Fixed';
 import AppErrorBoundary from './AppErrorBoundary';
 import './offline.css';
 import './accessibility.css';
