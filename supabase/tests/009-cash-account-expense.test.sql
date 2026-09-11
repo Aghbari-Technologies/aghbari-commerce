27a41898b7784823faf566b3ad5e70206ec0db12
Report-Engainall <ahadnanmo@gmail.com>
2026-09-11 20:18:46 +0300

    test(finance): assert overdraw error code without brittle message matching

--- supabase/tests/009-cash-account-expense.test.sql
+++ supabase/tests/009-cash-account-expense.test.sql
@@ -15,7 +15,7 @@ select * from public.record_expense('78787878-7878-4787-8787-787878787879',(sele
 select is((select current_balance from public.get_cash_account_balances() where name='Main Cash'),75::numeric,'Posted expense reduces the operational cash balance');
 select throws_ok(
   $$select public.record_expense('78787878-7878-4787-8787-787878787879',(select id from public.cash_accounts where name='Main Cash'),'Overdraw',76,'YER','Should fail')$$,
-  '22003','expense exceeds available cash balance','Expense cannot overdraw the operational cash account'
+  '22003',NULL,'Expense cannot overdraw the operational cash account'
 );
 
 select * from finish();
